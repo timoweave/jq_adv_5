336184319b38b5349f52a299b8c65ceabc9569bb
Timothy Shiu <timoshiu@yahoo.com>
2015-08-01 18:11:04 -0700

    + 1 tab, 3 accordions

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function() {
     function init() {
-        $("#my-tabs").tabs();
+        var tab_block = $("#my-pets");
+        var tab_detail = tab_block.children("div").children(["#my-cats-accordion", "#my-fishes-accordion", "#my-birds-accordion"]);
+
+        tab_block.tabs();
+        tab_detail.each(function() {
+            $(this).accordion({collapsible: true, active: false});
+        });
     }
+
+    init();
 });
