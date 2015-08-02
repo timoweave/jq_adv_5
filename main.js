089459235445d8c435d0afc25339f7d510174981
Timothy Shiu <timoshiu@yahoo.com>
2015-08-01 18:50:21 -0700

    fix accordion's height

--- main.js
+++ main.js
@@ -6,7 +6,18 @@ $(document).ready(function() {
 
         tab_block.tabs();
         tab_detail.each(function() {
-            $(this).accordion({collapsible: true, active: false});
+            $(this).accordion({collapsible: true, active: false, heightStyle: "auto"});
+            $(this).on( "accordionbeforeactivate", function( event, ui ) {
+                // NOTE: for some unknown reason, 
+                // the only the 1st tab's accordion has non-zero height
+                // and other tab's accordion has zero height.
+                // use accordionbeforeactivate to recompute the hight 
+                // TBD : how to just recompute the height just once?
+                $(this).accordion("option", "heightStyle", "auto");
+                $(this).accordion("refresh");
+
+            });
+
         });
     }
 
